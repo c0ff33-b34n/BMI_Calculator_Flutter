ce538e813c15dd86e582acdbe87ec72f880cd301
Stuart Bowes <stuartbowes2000@gmail.com>
2022-03-09 11:01:26 +0000

    Refactor ResuableCard to take color in its constructor to set card color

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -19,25 +19,25 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReusableCard(),
+                    child: ReusableCard(Color(0xFF1D1E33)),
                   ),
                   Expanded(
-                    child: ReusableCard(),
+                    child: ReusableCard(Color(0xFF1D1E33)),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: ReusableCard(),
+              child: ReusableCard(Color(0xFF1D1E33)),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReusableCard(),
+                    child: ReusableCard(Color(0xFF1D1E33)),
                   ),
                   Expanded(
-                    child: ReusableCard(),
+                    child: ReusableCard(Color(0xFF1D1E33)),
                   ),
                 ],
               ),
@@ -50,13 +50,17 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
+  final Color colour;
+
+  ReusableCard(@required this.colour);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xFF1D1E33),
+        color: colour,
       ),
     );
   }
